a67af17a793ef386079afc092120c8512a75b85a
Chandler <kleinc16@gmail.com>
2020-04-18 20:27:14 -0400

    rounded input to 4 decimal places

--- qsolve.R
+++ qsolve.R
@@ -1,85 +1,81 @@
-# Chandler Klein
-# Qsolve Function
-#
-# The function qsolve(a, b, c)
-# solves the quadratic equation
-#     a x^2 + b x + c = 0
-# for real solutions x1 and x2
-
-# Input:
-#     a, b and c are finite numeric real numbers
-#     a != 0
-#     length of c is not greater than 1
-
-# Return:
-#     c(x1, x2) where x1 and x2 are finite 
-#               numeric real numbers
-#               x1 <= x2
-#     NA        if the input is not as expected or 
-#               the return values can not be computed 
-#               as expected. 
+# TYPE qsolve() TO RUN
 
 library(testthat)
 
-qsolve <- function() {
+qsolve <- function() 
+{
   writeLines(readLines("title.txt"))
-  
   list <- getInput()
   a <- list[[1]]
   b <- list[[2]]
   c <- list[[3]]
-  
   qsolveR(a, b, c)
 }
 
-qsolveR <- function(a,b,c) {
-  
-  if (!is.finite(a) || !is.finite(b) || !is.finite(c)) {
+qsolveR <- function(a,b,c) 
+{
+  if (!is.finite(a) || !is.finite(b) || !is.finite(c))
     return(NA)
-  }
   
-  if (a == 0) {
+  if (a == 0) 
     return(NA)
-  }
   
-  if (length(c) > 1) {
-    return(NA)
-  }
+  a <- roundTo4(a)
+  b <- roundTo4(b)
+  c <- roundTo4(c)
   
-  disc <- b^2 - 4*a*c
-  if (is.nan(disc)) {
+  disc <- b*b - 4*a*c
+  
+  if (is.nan(disc))
     disc = 1
-  }
-  if (disc < 0) {
+
+  if (disc < 0) 
     return(NA)
-  }
   
   x1 <- (-b - sqrt(disc))/(2*a)
   x2 <- (-b + sqrt(disc))/(2*a)
   
-  if (is.infinite(x1) || is.infinite(x2)) {
+  if (is.infinite(x1) || is.infinite(x2)) 
     return(NA)
-  }
   
-  if (is.vector(c(x1, x2))) {
-    if (x1 > x2) {
-      return (c(x2, x1))
-    } else {
-      return (c(x1, x2))
-    }
-  } else {
-    return (NA)
-  }
-  
+  # Return the roots in order by value
+  if (is.vector(c(x1, x2))) 
+    return(result <- if(x1 > x2) c(x2,x1) else c(x1,x2))
+  else 
+    return(NA)
 }
 
-getInput <- function() {
+getInput <- function() 
+{
   a <- readline(prompt="Enter a: ");
   b <- readline(prompt="Enter b: ");
   c <- readline(prompt="Enter c: ");
   
-  inputs <- suppressWarnings(as.double(c(a, b, c)))
+  inputs <- suppressWarnings(as.double(c(a,b,c)))
   return(inputs)
 }
 
-test_file('tests/testthat/qstest_file.R')
+roundTo4 <- function(x)
+{
+  if (decimalPlaces(x) > 4)
+  {
+    x <- round(x, digits=4)
+    cat(">> Value rounded to", x, "\n")
+    return(x)
+  }
+  else
+    return(x)
+}
+
+decimalPlaces <- function(x)
+{
+  if (abs(x - round(x)) > .Machine$double.eps^0.5)
+    nchar(strsplit(sub('0+$', '', as.character(x)), ".", fixed=TRUE)[[1]][[2]])
+  else
+    return(0)
+}
+
+test <- function()
+{
+  test_file('tests/testthat/qstest_file.R')
+}
